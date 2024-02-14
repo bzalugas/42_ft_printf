a0f00a918d0d7d9b1c71507848a276bc7eed771a
bazaluga <bazaluga@student.42.fr>
2024-02-14 07:16:08 +0100

    Add abs function

--- src/utils.c
+++ src/utils.c
@@ -6,9 +6,15 @@
 /*   By: bazaluga <bazaluga@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/08 18:21:03 by bazaluga          #+#    #+#             */
-/*   Updated: 2024/02/12 13:00:31 by bazaluga         ###   ########.fr       */
+/*   Updated: 2024/02/13 21:21:19 by bazaluga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/ft_printf.h"
 
+unsigned int	ft_abs(int n)
+{
+	if (n < 0)
+		return (-n);
+	return (n);
+}
