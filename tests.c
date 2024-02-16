f4cdd2a8f8b17b299b3aa1719877a722578a7140
bazaluga <bazaluga@student.42.fr>
2024-02-16 18:50:33 +0100

    Begin some archaics tests

--- tests.c
+++ tests.c
@@ -4,11 +4,46 @@
 #include <string.h>
 #include "includes/ft_printf.h"
 
+int	tests()
+{
+	int	r1;
+	int	r2;
+
+	fflush(stdout);
+	r1 = printf(" <%c> \n", 'a');
+	fflush(stdout);
+	r2 = ft_printf(" <%c> \n", 'a');
+	if (r1 != r2)
+		return 0;
+	fflush(stdout);
+	r1 = printf(" <%5c> \n", 'a');
+	fflush(stdout);
+	r2 = ft_printf(" <%5c> \n", 'a');
+	if (r1 != r2)
+		return 0;
+	fflush(stdout);
+	r1 = printf(" <%-5c> \n", 'a');
+	fflush(stdout);
+	r2 = ft_printf(" <%-5c> \n", 'a');
+	if (r1 != r2)
+		return 0;
+	fflush(stdout);
+	r1 = printf(" <%c%c%c> \n", 'a', 'b', 'c');
+	fflush(stdout);
+	r2 = ft_printf(" <%c%c%c> \n", 'a', 'b', 'c');
+	if (r1 != r2)
+		return 0;
+	return 1;
+}
+
 int	main(void)
 {
-	return (ft_printf("%.5d", -23));
-	/* return (printf("%.5d", -23)); */
-	/* return (printf("%-10.1s", "abcde")); */
-	/* printf("%d\n", 15 % 10); */
+	if (!tests())
+	{
+		printf("\n\nERROR\n");
+		return (1);
+	}
+	printf("\n\nALL GOOD\n");
+	/* ft_printf("%10d", 23); */
 	return (0);
 }
